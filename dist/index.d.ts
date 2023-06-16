15695441ff9e71bd2c84e19322d06d4d27dd478b
Doug A <douganderson444@gmail.com>
2023-06-16 13:06:49 -0300

    export libp2p functions

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,3 @@
-export { default as Chat, default } from "./components/Chat.svelte";
+export { default as Chat } from './components/Chat.svelte';
+export { default as default } from './components/Chat.svelte';
+export * from './p2p/libp2p';
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 // Reexport your entry components here
 export { default as Chat } from './components/Chat.svelte';
 export { default as default } from './components/Chat.svelte';
+export * from './p2p/libp2p';
